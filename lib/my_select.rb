2e64f15d65b89c7ef03e9d91a4f5d4c7a65ad1e5
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 03:00:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
  i = 0
  array = []
  while i < collection.length
-   array.push(yield collection[i])
+   if yield collection[i]
+     array.push
+   end
    i += 1
  end
  array
